2ab64086ab4fe130bfdfd2ffbc305652e7e9f5d8
Nashir Wahyudi <nashirwahyudi@gmail.com>
2022-11-11 00:52:00 +0700

    wdawda

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -48,6 +48,35 @@ function showLoading(show) {
   }
 }
 
+function resolveClusterColor(index, feature) {
+  let props = "cluster";
+  if (selectedLayer != "") {
+    props = selectedLayer;
+  }
+  let colorScheme = {
+    r: 12 / 255,
+    g: 83 / 255,
+    b: 147 / 255,
+    a: 0.4,
+  };
+  if (feature.properties[props] == 2) {
+    colorScheme = {
+      r: 230 / 255,
+      g: 145 / 255,
+      b: 56 / 255,
+      a: 0.4,
+    };
+  } else if (feature.properties[props] == 3) {
+    colorScheme = {
+      r: 146 / 255,
+      g: 196 / 255,
+      b: 124 / 255,
+      a: 0.4,
+    };
+  }
+  return colorScheme;
+}
+
 function resolveColor(index, feature) {
   let props = "IKP_PRED";
   if (selectedLayer != "") {
@@ -235,42 +264,142 @@ const gridLayer2021 = L.glify.layer({
   },
 });
 
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const clusterLayer2021 = L.glify.layer({
+  geojson: cluster2021,
+  paneName: "Cluster 2021",
+  glifyOptions: {
+    color: resolveClusterColor,
+    size: 30,
+    opacity: 0.8,
+    click(e, feature, xy) {
+      if (Array.isArray(feature)) {
+        L.popup().setContent(`You clicked on a point`).openOn(map);
+      } else {
+        L.popup({
+          minWidth: 300,
+        })
+          .setLatLng(e.latlng)
+          .setContent(
+            `<ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Cluster <span class="badge rounded-pill bg-primary">${
+                      feature.properties.cluster
+                    }</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Availability <span class="badge rounded-pill bg-info">${feature.properties.Ketersediaan_PRED.toFixed(
+                      2
+                    )}</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Accessibility <span class="badge rounded-pill bg-info">${feature.properties.Keterjangkauan_PRED.toFixed(
+                      2
+                    )}</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Utilization
+                    <span class="badge rounded-pill bg-info">${feature.properties.Pemanfaatan_PRED.toFixed(
+                      2
+                    )}</span>
+                </li>
+                </ul>
+            `
+          )
+          .openOn(map);
+      }
+    },
+    hover(e, feature) {
+      console.log("hover", feature);
+    },
+  },
+  onAdd() {
+    showLoading(true);
+  },
+  onLayersInit() {
+    showLoading(false);
+  },
+  onRemove() {
+    console.log("onRemove callback");
+  },
+});
+
+const clusterLayer2022 = L.glify.layer({
+  geojson: cluster2022,
+  paneName: "Cluster 2022",
+  glifyOptions: {
+    color: resolveClusterColor,
+    size: 30,
+    opacity: 0.8,
+    click(e, feature, xy) {
+      if (Array.isArray(feature)) {
+        L.popup().setContent(`You clicked on a point`).openOn(map);
+      } else {
+        L.popup({
+          minWidth: 300,
+        })
+          .setLatLng(e.latlng)
+          .setContent(
+            `<ul class="list-group">
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Cluster <span class="badge rounded-pill bg-primary">${
+                        feature.properties.cluster
+                      }</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Availability <span class="badge rounded-pill bg-info">${feature.properties.Ketersediaan_PRED.toFixed(
+                        2
+                      )}</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Accessibility <span class="badge rounded-pill bg-info">${feature.properties.Keterjangkauan_PRED.toFixed(
+                        2
+                      )}</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Utilization
+                      <span class="badge rounded-pill bg-info">${feature.properties.Pemanfaatan_PRED.toFixed(
+                        2
+                      )}</span>
+                  </li>
+                  </ul>
+              `
+          )
+          .openOn(map);
+      }
+    },
+    hover(e, feature) {
+      console.log("hover", feature);
+    },
+  },
+  onAdd() {
+    showLoading(true);
+  },
+  onLayersInit() {
+    showLoading(false);
+  },
+  onRemove() {
+    console.log("onRemove callback");
+  },
+});
+
 const layerControl = new L.control.layers(
   {
     "Grid FSI 2022": gridLayer2022,
     "Grid FSI 2021": gridLayer2021,
+    "Cluster FSI 2022": clusterLayer2022,
+    "Cluster FSI 2021": clusterLayer2021,
   },
   {},
   { collapsed: false }
 );
 
 layerControl.addTo(map);
-
-var legend = L.control({ position: "bottomleft" });
-
-legend.onAdd = function (map) {
-  var div = L.DomUtil.create("div", "legend");
-  div.innerHTML += "<h4>Color Range</h4>";
-  div.innerHTML +=
-    '<i style="background: #249A44"></i><span> 80 < FSI <= 100 </span><br>';
-  div.innerHTML +=
-    '<i style="background: #A0D97B"></i><span> 75 < FSI <= 80</span><br>';
-  div.innerHTML +=
-    '<i style="background: #DFF1A3"></i><span> 70 < FSI <= 75</span><br>';
-  div.innerHTML +=
-    '<i style="background: #FFFFB1"></i><span> 65 < FSI <= 70 </span><br>';
-  div.innerHTML +=
-    '<i style="background: #F69657"></i><span> 60 < FSI <= 65 </span><br>';
-  div.innerHTML +=
-    '<i style="background: #DE332C"></i><span> FSI <= 60 </span><br>';
-  return div;
-};
-// layerControl.addTo(map);
-legend.addTo(map);
-
-map.on("baselayerchange", function (e) {
-  baseLayer = e.layer;
-});
 
 function onSelectionChange(e) {
   selectedLayer = document.getElementById("metrics-select").value;
@@ -342,4 +471,75 @@ function handleCheckboxChange(e) {
 var checkboxElems = document.querySelectorAll(".marker-checkbox");
 checkboxElems?.forEach((elem) => {
   elem.addEventListener("change", handleCheckboxChange);
+});
+
+let legend = L.control({ position: "bottomleft" });
+let legendCluster = L.control({ position: "bottomleft" });
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create("div", "legend");
+  div.innerHTML += "<h4>Color Range</h4>";
+  div.innerHTML +=
+    '<i style="background: #249A44"></i><span> 80 < FSI <= 100 </span><br>';
+  div.innerHTML +=
+    '<i style="background: #A0D97B"></i><span> 75 < FSI <= 80</span><br>';
+  div.innerHTML +=
+    '<i style="background: #DFF1A3"></i><span> 70 < FSI <= 75</span><br>';
+  div.innerHTML +=
+    '<i style="background: #FFFFB1"></i><span> 65 < FSI <= 70 </span><br>';
+  div.innerHTML +=
+    '<i style="background: #F69657"></i><span> 60 < FSI <= 65 </span><br>';
+  div.innerHTML +=
+    '<i style="background: #DE332C"></i><span> FSI <= 60 </span><br>';
+  return div;
+};
+// layerControl.addTo(map);
+legend.addTo(map);
+
+map.on("baselayerchange", function (e) {
+  baseLayer = e.layer;
+  const select = document.getElementById("metrics");
+  if (e.layer._paneName?.includes("Cluster")) {
+    // remove legend range and add cluster legend
+    map.removeControl(legend);
+    map.removeControl(legendCluster);
+    select.classList.add("d-none");
+
+    legendCluster.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "legend");
+      div.innerHTML += "<h4>Cluster Profile</h4>";
+      div.innerHTML +=
+        '<i style="background: #92C47C"></i><span> 3 </span><br>';
+      div.innerHTML +=
+        '<i style="background: #E69138"></i><span> 2 </span><br>';
+      div.innerHTML +=
+        '<i style="background: #0C5393"></i><span> 1 </span><br>';
+      return div;
+    };
+
+    legendCluster.addTo(map);
+  } else {
+    map.removeControl(legendCluster);
+    map.removeControl(legend);
+    select.classList.add("d-inline");
+
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "legend");
+      div.innerHTML += "<h4>Color Range</h4>";
+      div.innerHTML +=
+        '<i style="background: #249A44"></i><span> 80 < FSI <= 100 </span><br>';
+      div.innerHTML +=
+        '<i style="background: #A0D97B"></i><span> 75 < FSI <= 80</span><br>';
+      div.innerHTML +=
+        '<i style="background: #DFF1A3"></i><span> 70 < FSI <= 75</span><br>';
+      div.innerHTML +=
+        '<i style="background: #FFFFB1"></i><span> 65 < FSI <= 70 </span><br>';
+      div.innerHTML +=
+        '<i style="background: #F69657"></i><span> 60 < FSI <= 65 </span><br>';
+      div.innerHTML +=
+        '<i style="background: #DE332C"></i><span> FSI <= 60 </span><br>';
+      return div;
+    };
+    // layerControl.addTo(map);
+    legend.addTo(map);
+  }
 });
